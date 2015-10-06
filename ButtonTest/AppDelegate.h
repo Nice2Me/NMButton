48965097bebb5b70edb061d42b7008122a775020
liuzuopeng01 <liuzuopeng01@baidu.com>
2015-10-06 23:25:57 +0800

    define textImageButton

---
+++ ButtonTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ButtonTest
+//
+//  Created by zuopengl on 10/6/15.
+//  Copyright © 2015 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ButtonTest/ViewController.m
@@ -0,0 +1,54 @@
+//
+//  ViewController.m
+//  ButtonTest
+//
+//  Created by zuopengl on 10/6/15.
+//  Copyright © 2015 Apple. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "UITextImageButton.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    
+    UITextImageButton *button = [UITextImageButton buttonWithTitle:@"支付" image:[UIImage imageNamed:@"icon_arrows_gray_right"]];
+    button.frame = CGRectMake(100, 100, 100, 100);
+    [button setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [button setTitleImageType:kUITextImageButtonTypeLeftText];
+    [self.view addSubview:button];
+    
+    UITextImageButton *button2 = [UITextImageButton buttonWithTitle:@"支付" image:[UIImage imageNamed:@"icon_arrows_gray_right"]];
+    button2.frame = CGRectMake(100, 200, 100, 100);
+    [button2 setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [button2 setTitleImageType:kUITextImageButtonTypeRightText];
+    [self.view addSubview:button2];
+    
+    UITextImageButton *button3 = [UITextImageButton buttonWithTitle:@"支付" image:[UIImage imageNamed:@"icon_arrows_gray_right"]];
+    button3.frame = CGRectMake(100, 300, 100, 100);
+    [button3 setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [button3 setTitleImageType:kUITextImageButtonTypeTopText];
+    [self.view addSubview:button3];
+    
+    
+    UITextImageButton *button4 = [UITextImageButton buttonWithTitle:@"支付" image:[UIImage imageNamed:@"icon_arrows_gray_right"]];
+    button4.frame = CGRectMake(100, 400, 100, 100);
+    [button4 setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [button4 setTitleImageType:kUITextImageButtonTypeBottomText];
+    [self.view addSubview:button4];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
